d7e6e7d7e91859b0e744e05fd9e08195e7888ea1
Tony McNeil <mcneil.tony@gmail.com>
2022-03-26 21:24:38 +1000

    refactor, add FIXME marker

--- v-default.sh
+++ v-default.sh
@@ -2,4 +2,5 @@
 
 source .common.sh
 
+./kill-all.sh
 ffmpeg -i $REAL_CAM -f v4l2 -vcodec rawvideo -pix_fmt yuv420p $VIRTUAL_CAM >/dev/null 2>&1 &
--- v-pixel.sh
+++ v-pixel.sh
@@ -4,4 +4,5 @@ source .common.sh
 
 # ffmpeg -i $REAL_CAM -vf "frei0r=filter_name=pixeliz0r:filter_params=0.02|0.02" -f v4l2 -vcodec rawvideo -pix_fmt yuv420p $VIRTUAL_CAM  >/dev/null 2>&1 &
 
+./kill-all.sh
 ffmpeg -i $REAL_CAM -vf "frei0r=filter_name=pixeliz0r" -f v4l2 -vcodec rawvideo -pix_fmt yuv420p $VIRTUAL_CAM  >/dev/null 2>&1 &
